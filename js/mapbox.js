ae0b31a4ffd54c29c127fae3216b59d1197e9693
Ergis <ehotza35@gmail.com>
2021-12-18 14:06:29 +0200

    Fixes5

--- js/mapbox.js
+++ js/mapbox.js
@@ -99,6 +99,7 @@ map.on('load', () => {
 
     // Get coordinates from the api every 5 seconds.
     window.setInterval(function () {
+        map.getSource('dot-point').setData('https://pets-backend-api.herokuapp.com/geo/get');
         const res = fetch('https://pets-backend-api.herokuapp.com/geo/get')
             .then(response => response.json())
             .then(data => foles = data)
